a1842b72f2dd7e695627e7e1b353cc713ee6574a
Renato Rocha <renatob.rocha@gmail.com>
2020-11-25 16:12:24 -0300

    Animation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,80 +1,35 @@
 import React, { useRef } from 'react';
 import { Dimensions, StyleSheet, View } from 'react-native';
 import Animated, {
-  and,
   block,
   Clock,
   clockRunning,
   cond,
-  debug,
   Easing,
   eq,
   Extrapolate,
   interpolate,
   not,
-  onChange,
   set,
   startClock,
-  stopClock,
   timing,
 } from 'react-native-reanimated';
-import { Circle } from './Circle';
 
 const { width } = Dimensions.get('window');
 
-const COLORS = ['#FF6E02', '#E56300', '#B34D02'];
+const COLORS = ['#B34D02', '#E56300', '#FF6E02'];
+const SIZE = 40;
+const MARGIN = 30;
+const CENTER = width / 2 - SIZE / 2;
+const DELTA = SIZE + MARGIN;
+const first = CENTER - SIZE - MARGIN;
+const third = CENTER + SIZE + MARGIN;
 
-const scaleWithTiming = (
-  clock: Clock,
-  intialPosition: Animated.Value<number>,
-  toValue: Animated.Value<number>,
-  duration: number,
-  invert: Animated.Value<number>,
-  startTranslateX: Animated.Value<number>,
-  startScale: Animated.Value<number>
-) => {
-  const state = {
-    finished: new Animated.Value(0),
-    position: intialPosition,
-    frameTime: new Animated.Value(0),
-    time: new Animated.Value(0),
-  };
-
-  const config = {
-    toValue,
-    duration,
-    easing: Easing.inOut(Easing.linear),
-  };
-
-  return block([
-    // cond(invert, [
-    //   set(state.position, new Animated.Value(toValue)),
-    //   set(config.toValue, new Animated.Value(intialPosition)),
-    // ]),
-    cond(clockRunning(clock), timing(clock, state, config)),
-
-    cond(eq(state.finished, 1), [
-      stopClock(clock),
-      set(state.finished, 0),
-      set(state.frameTime, 0),
-      set(state.time, 0),
-      set(invert, not(invert)),
-      set(startTranslateX, 1),
-      // set(startScale, 0),
-      // debug('startScale SCALE', startScale),
-    ]),
-    state.position,
-  ]);
-};
-
-const translateWithTiming = (
+const runProgress = (
   clock: Clock,
   intialPosition: number,
   toValue: number,
-  duration: number,
-  startTranslateX: Animated.Value<number>,
-  startScale: Animated.Value<number>[],
-  index: number
+  duration: number
 ) => {
   const state = {
     finished: new Animated.Value(0),
@@ -90,51 +45,109 @@ const translateWithTiming = (
   };
 
   return block([
-    cond(clockRunning(clock), timing(clock, state, config)),
+    cond(not(clockRunning(clock)), startClock(clock)),
+    timing(clock, state, config),
     cond(eq(state.finished, 1), [
-      stopClock(clock),
       set(state.finished, 0),
       set(state.frameTime, 0),
-      set(state.time, 0),
-      set(startTranslateX, 0),
-      set(startScale[index], 1),
-      debug('startTranslateX[index]', startTranslateX),
-      // debug('startScale TRANS', startScale),
+      set(state.position, intialPosition),
     ]),
     state.position,
   ]);
 };
 
 const Loading: React.FC = () => {
+  const clocks = useRef(COLORS.map((_) => new Clock())).current;
+
+  const progress = clocks.map((clock, i) =>
+    runProgress(clock, 0, 10, i === 1 ? 1000 : 2000)
+  );
+
   return (
     <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
       {COLORS.map((color, index) => {
-        const center = width / 2 - 40 / 2;
-        const left = center - 40 - 30;
-        const third = center + 40 + 30;
+        const alpha = index === 0 ? -1 : index === 1 ? 0 : 1;
+        const left = CENTER + alpha * DELTA;
 
-        if (index === 0) {
-          // left = center - 40 - 30;
-          // next = center;
-        } else if (index === 2) {
-          // left = center + 40 + 30;
-          // next = center - 40 - 30;
+        let inputRangeTransX = [0, 0.00001, 5, 5.000005, 9.00009, 10];
+        let outputRangeTransX = [
+          left,
+          left - 0.00001,
+          third,
+          third - 0.00001,
+          left - 0.00001,
+          left,
+        ];
+
+        let inputRangeScale = [0, 0.00001, 5.00009, 6, 9.00009, 10];
+        let outputRangeScale = [1, 1, 1, 0.1, 0.1, 1];
+
+        if (index === 1) {
+          inputRangeTransX = [0, 0.00001, 5, 5.000005, 7, 7.09, 9.999999, 10];
+          outputRangeTransX = [
+            left,
+            left - 0.00001,
+            third,
+            third - 0.00001,
+            first,
+            first - 0.00001,
+            left,
+            left - 0.00001,
+          ];
+
+          inputRangeScale = [0, 0.00001, 5, 5.2, 7, 7.09, 9.999999, 10];
+          outputRangeScale = [1, 1, 1, 0.1, 0.1, 1, 1, 1];
         }
 
-        // left = center - 40 - 30;
+        if (index === 2) {
+          inputRangeTransX = [0, 0.00001, 5, 5.000005, 9.00009, 10];
+          outputRangeTransX = [
+            left,
+            left - 0.00001,
+            first,
+            first - 0.00001,
+            left,
+            left - 0.00001,
+          ];
+
+          inputRangeScale = [0, 0.999, 5, 5.6, 9.00009, 10];
+          outputRangeScale = [1, 0.1, 0.1, 1, 1, 1];
+        }
+
+        const translateX = interpolate(progress[index], {
+          inputRange: inputRangeTransX,
+          outputRange: outputRangeTransX,
+          extrapolate: Extrapolate.CLAMP,
+        });
+
+        const scale = interpolate(progress[index], {
+          inputRange: inputRangeScale,
+          outputRange: outputRangeScale,
+          extrapolate: Extrapolate.CLAMP,
+        });
 
         return (
-          <Circle
+          <Animated.View
+            style={[
+              styles.circle,
+              { backgroundColor: color, left: translateX },
+              { transform: [{ scale }] },
+            ]}
             key={index.toString()}
-            color={color}
-            position={left}
-            beginState={index === 0 ? 1 : 0}
-            translateTo={third}
           />
         );
       })}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  circle: {
+    position: 'absolute',
+    height: SIZE,
+    width: SIZE,
+    borderRadius: SIZE / 2,
+  },
+});
 
 export { Loading };
